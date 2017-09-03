516059634698a2a39106c1dc125dae5d66a7debb
cschar <codyscharfe@gmail.com>
2017-09-02 21:56:11 -0400

    arven view: css grid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
--- app/assets/javascripts/arven.js
+++ app/assets/javascripts/arven.js
@@ -5,13 +5,19 @@ $(document).on('turbolinks:load', function(){
     console.log("turbolink loaded ===")
     if($('#arven').length) {
         console.log('on arven page');
-        // setArven()
+        setArven()
     }
 });
 
+var count = 0;
+var output = ''
+var markers = [];
+var dumps = '';
+
 
 function setArven(){
-    var map2 = L.map('map').fitWorld();
+    console.log('setting map in arven')
+    var map2 = L.map('arvenMap').fitWorld();
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
@@ -60,7 +66,18 @@ function setArven(){
         div.append(link1)
         div.append(link2)
 
-
+        // var dIcon = L.icon({
+        //     iconUrl: 'kerm.png',
+        //     // shadowUrl: 'kerm.png',
+        //
+        //     iconSize:     [40, 40], // size of the icon
+        //     // shadowSize:   [10, 24], // size of the shadow
+        //     iconAnchor:   [0, 40], // point of the icon which will correspond to marker's location
+        //     // shadowAnchor: [4, 62],  // the same for the shadow
+        //     popupAnchor:  [-3, -40] // point from which the popup should open relative to the iconAnchor
+        // });
+        //
+        // var marker = L.marker(e.latlng, { icon: dIcon});
         var marker = L.marker(e.latlng);
         marker.bindPopup(div[0]);
         marker.addTo(map2)
@@ -68,12 +85,18 @@ function setArven(){
     }
 
     map2.on('click', onClick)
+
+    var mapCallback = function(){
+        var center = map2.getCenter()
+        count = count + 1;
+        output = [count.toString() + center.toString(),
+            'markers : ' + markers.length.toString()
+        ]
+        $('#status').text(output)
+        $('#feed').text(clickSpot.toString())
+        $('#feed2').text(dumps)
+    }
+    setInterval(mapCallback, 2000);
+
 }
 
-$(function(){
-    console.log('arvenmap jquery init')
-    console.log($('#arven').length);
-    if($('#arven').length) { console.log('on arven page') }
-    //
-
-})
