a3820332fc7033e2ee9c3513047cc97a6262f4a5
cschar <codyscharfe@gmail.com>
2017-09-20 17:10:17 -0400

    bug

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,7 @@ Types::UserType = GraphQL::ObjectType.define do
   name 'User'
 
   field :id, !types.ID
-  # field :email, !types.String
+  field :email, !types.String
 
   # generate when user is made
   # https://github.com/usmanbashir/haikunator
