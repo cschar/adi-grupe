0c94dc99febfc87eebc7dc6597dc909a5a3aa436
cschar <codyscharfe@gmail.com>
2018-05-30 21:50:17 -0400

    hello react working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
-// import 'rpg-awesome';
 require('./maps');
 
 console.log('Hello World from Webpacker')
-
-
